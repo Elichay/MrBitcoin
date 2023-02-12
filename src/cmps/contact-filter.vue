9f2253426c89b1832067e3c64f457330ac9285fd
Elichay Kaplan <Elichay_Kaplan@Yahoo.com>
2023-02-13 00:46:33 +0200

    contacts filter

---
+++ src/cmps/contact-filter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="contact-filter">
+    <p>Search Contact:</p>
+    <input @input="onFilter" type="text" v-model="filterBy.name" placeholder="Search by name" />
+    <!-- <input type="text" v-model="filterBy.score" placeholder="Search by score" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterBy: {
+        name: '',
+      },
+    }
+  },
+    methods: {
+        onFilter() {
+        this.$emit('filter', this.filterBy)
+        },
+    },
+}
+</script>
+
+<style>
+</style>
--- src/pages/contact-index.vue
+++ src/pages/contact-index.vue
@@ -1,19 +1,21 @@
 <template>
-  <!-- <div class="contact-index"> -->
-  <!-- <h1>Contact Index</h1> -->
-  <RouterLink to="/contact/edit"><button>Add Contact</button> </RouterLink>
-  <ContactList @remove="removeContact" v-if="contacts" :contacts="contacts" />
-
-  <!-- </div> -->
+  <div class="contacts-actions">
+    <ContactFilter @filter="OnSetFilterBy"/>
+    <RouterLink to="/contact/edit"><button>Add Contact</button> </RouterLink>
+  </div>
+  <ContactList @remove="removeContact" v-if="contacts" :contacts="filteredContacts" />
 </template>
 
 <script>
 import { contactService } from "@/services/contact.service.js";
 import ContactList from "@/cmps/contact-list.vue";
+import ContactFilter from "@/cmps/contact-filter.vue";
+
 export default {
   data() {
     return {
       contacts: null,
+      filterBy: {},
     };
   },
   async created() {
@@ -26,9 +28,21 @@ export default {
         (contact) => contact._id !== contactId
       );
     },
+    OnSetFilterBy(filterBy) {
+      this.filterBy = filterBy;
+      console.log('this.filterBy', this.filterBy)
+    },
+  },
+  computed: {
+    filteredContacts() {
+      const regex = new RegExp(this.filterBy.name, 'i');
+      return this.contacts.filter(contact => 
+        regex.test(contact.name))
+    },
   },
   components: {
     ContactList,
+    ContactFilter,
   },
 };
 </script>
